9c1acce49f1dc45228aa6777f2d77b5e4298f46c
Peter Bui <pbui@nd.edu>
2020-07-28 15:46:50 -0400

    reading01: add test program

---
+++ reading01/test_program.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+output() {
+    cat <<EOF
+False
+False
+True
+True
+False
+True
+True
+False
+True
+False
+EOF
+}
+
+printf "Testing Reading 01 program ... "
+
+DIFF=$(diff <(./program 2> /dev/null) <(output) | grep "^>" | wc -l)
+SCORE=$(echo "scale=2; (10 - $DIFF) * 3.0 / 10.0" | bc)
+if [ "$DIFF" -eq 0 ]; then
+    echo "Success $(SCORE)"
+    exit 0
+else
+    echo "Failure ($SCORE)"
+    exit $DIFF
+fi
