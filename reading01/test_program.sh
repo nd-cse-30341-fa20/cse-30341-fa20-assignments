c9bcf6b435930db9109cc71505211a79c80e0826
Peter Bui <pbui@nd.edu>
2020-07-28 16:09:29 -0400

    reading01: update test script

--- reading01/test_program.sh
+++ reading01/test_program.sh
@@ -20,7 +20,7 @@ printf "Testing reading01 program ... "
 DIFF=$(diff <(./program 2> /dev/null) <(output) | grep "^>" | wc -l)
 SCORE=$(echo "scale=2; (10 - $DIFF) * 3.0 / 10.0" | bc)
 if [ "$DIFF" -eq 0 ]; then
-    echo "Success $(SCORE)"
+    echo "Success ($SCORE)"
     exit 0
 else
     echo "Failure ($SCORE)"
