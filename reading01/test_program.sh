f32313c0c794261adb64c7b836f272269773d87d
Peter Bui <pbui@nd.edu>
2020-07-28 16:12:34 -0400

    reading01: refactor test script

--- reading01/test_program.sh
+++ reading01/test_program.sh
@@ -21,8 +21,8 @@ DIFF=$(diff <(./program 2> /dev/null) <(output) | grep "^>" | wc -l)
 SCORE=$(echo "scale=2; (10 - $DIFF) * 3.0 / 10.0" | bc)
 if [ "$DIFF" -eq 0 ]; then
     echo "Success ($SCORE)"
-    exit 0
 else
     echo "Failure ($SCORE)"
-    exit $DIFF
 fi
+    
+exit $DIFF
