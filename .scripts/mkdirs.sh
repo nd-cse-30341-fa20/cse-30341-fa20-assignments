fb56b3656f3a824cdbe3ba83d2ee01d10f17cbc1
Peter Bui <pbui@nd.edu>
2020-07-28 14:58:49 -0400

    Add Makefile infrastructure to readings

--- .scripts/mkdirs.sh
+++ .scripts/mkdirs.sh
@@ -7,6 +7,37 @@ for i in $(seq 1 12); do
     cat > reading$n/README.md <<EOF
 # Reading $n
 EOF
+
+    cat > reading$n/Makefile <<EOF
+CC=	gcc
+CFLAGS=	-Wall -g -std=gnu99
+LIBS=	
+
+test:	test-quiz test-program
+
+program:    program.c
+	\$(CC) \$(CFLAGS) -o \$@ $^ \$(LIBS)
+
+test-quiz:
+
+test-program: program
+	curl -sLO https://github.com/nd-cse-30341-fa20/cse-30341-fa20-assignments/raw/master/test_program.sh
+	chmod +x test_program.sh
+	./test_program.sh
+
+clean:
+	rm -f program
+EOF
+    cat > reading$n/program.c <<EOF
+/* Reading $n */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
+EOF
 done
 
 for p in $(seq 1 4); do
