ece856ec5cf4515a7479b08c01ff4a4bcea5c250
Peter Bui <pbui@nd.edu>
2020-08-15 22:44:40 -0400

    reading04: add test script

---
+++ reading04/test_program.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+output() {
+    base64 -d \
+	<<<cHliZnIgZXJucSBiY3JhCmJjcmEKYmNyYQpweWJhcgpweWJhcgpqbnZnNAo= | \
+	tr 'A-Za-z' 'N-ZA-Mn-za-m'
+}
+
+printf "Testing reading01 program ... "
+
+DIFF=$(diff <(./program 2> /dev/null) <(output) | grep "^>" | wc -l)
+SCORE=$(python3 <<EOF
+print("{:0.2f}".format((6 - $DIFF) * 3.0 / 6.0))
+EOF
+)
+if [ "$DIFF" -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failure"
+fi
+
+echo "   Score $SCORE"
+exit $DIFF
